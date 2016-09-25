1c9f4f0d1ef3ccd28f5bc0715aa87569b2f2fd20
jacobmmm <jacob.42193@gmail.com>
2016-09-25 17:46:14 +0530

    first

---
+++ src/main/java/com/concretepage/controller/LoginController.java
@@ -0,0 +1,41 @@
+package com.concretepage.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+@Controller
+public class LoginController {
+	@RequestMapping("/")
+	public String get()
+	{
+		return "index";
+	}
+	
+	@RequestMapping("/success")
+	public String get1()
+	{
+		return "success";
+	}
+	
+	
+	/*@RequestMapping(value="login", method = RequestMethod.GET)
+	public String login(){
+		return "redirect:pages/index.jsp";
+	}
+	@RequestMapping(value="pages/userCheck", method = RequestMethod.POST)
+	public String userCheck(ModelMap model, HttpServletRequest request) {
+		String name=request.getParameter("name");
+		String pwd=request.getParameter("pwd");
+		if("concretepage".equalsIgnoreCase(name)&&"concretepage".equalsIgnoreCase(pwd)){
+			model.addAttribute("message", "Successfully logged in.");
+			
+		}else{
+			model.addAttribute("message", "Username or password is wrong.");
+		}
+		return "redirect:success.jsp";
+	}
+*/}
+ 
+ 
